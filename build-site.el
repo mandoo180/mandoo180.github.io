884f25744cf65c77226f32491a52a88fd5995af4
Kyeong Soo Choi <mandoo180@gmail.com>
2024-12-23 13:04:48 +0900

    fix: build-emacs.org added (needs more fixes)

--- build-site.el
+++ build-site.el
@@ -16,17 +16,35 @@
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head k-html-head)
+(setq org-hide-emphasis-markers t)
+(setq org-confirm-babel-evaluate nil)
+(setq org-use-property-inheritance t)
+(setq org-babel-min-lines-for-block-output 0)
+(org-babel-do-load-languages 'org-babel-load-languages '((shell . t) (emacs-lisp . t)))
 (setq org-publish-project-alist
       (list (list "org-site:main"
                   :recursive t
+                  :base-extension "org"
                   :base-directory "./content"
                   :publishing-directory "./public"
                   :publishing-function 'org-html-publish-to-html
                   :with-author t
                   :with-creator t
                   :with-toc t
+                  :with-latex t
                   :section-number nil
                   :time-stamp-file nil)))
+
+(defun run-before-org-export-processing (backend)
+  (message "Testing %s" backend)
+  (dolist (li (org-babel-src-block-names))
+  (if (string-equal "startup" li)
+      (progn
+        (org-babel-goto-named-src-block li)
+        (org-babel-execute-src-block)))))
+
+(setq org-export-before-processing-functions #'run-before-org-export-processing)
+
 
 (org-publish-all t)
 
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 emacs -Q --script build-site.el
+sudo cp ./public/* /var/www/html/
