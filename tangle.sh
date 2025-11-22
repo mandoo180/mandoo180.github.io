38ca3b1d55235a11c877d010cf98ffae8f0ad92d
Kyeong Soo Choi <mandoo180@gmail.com>
2025-11-22 11:57:12 +0900

    Restruct themes directory, change sicp.org file

--- tangle.sh
+++ tangle.sh
@@ -4,6 +4,13 @@
 #
 
 echo "Tangling all configuration files..."
+if [ ! -d ~/.emacs.d/elisp ]; then
+	echo "Elisp directory does not exist."
+	echo 'Making ~/.emacs.d/elisp/settings...'
+	mkdir -p ~/.emacs.d/elisp/settings
+	echo 'Making ~/.emacs.d/elisp/themes...'
+	mkdir -p ~/.emacs.d/elisp/themes
+fi
 emacs --batch --load tangle-configs.el
 
 echo "Done! Configuration files have been generated in ~/.emacs.d/"
